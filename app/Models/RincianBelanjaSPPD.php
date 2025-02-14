b964287c68083236aa80ebf0b98b3f1102bb56fb
Ryan Prabowo, S.T <ryanprabowo96@gmail.com>
2025-02-14 10:55:13 +0800

    log

--- app/Models/RincianBelanjaSPPD.php
+++ app/Models/RincianBelanjaSPPD.php
@@ -6,6 +6,7 @@ use DB;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Log;
 
 class RincianBelanjaSppd extends Model
 {
